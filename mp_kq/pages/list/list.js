265d7ce8fda5391a2dfebfb6a081d0c08d613581
寇诗卉koush <koush@digiwin.com>
2019-09-12 15:53:10 +0800

    1
    1

--- mp_kq/pages/list/list.js
+++ mp_kq/pages/list/list.js
@@ -7,7 +7,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    base64:{},
+    base64: {},
     listDatas: [
       {
         text: '标题文本',
@@ -24,7 +24,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({ base64})
+    this.setData({ base64 })
   },
 
   /**
