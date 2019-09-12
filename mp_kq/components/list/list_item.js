c09427f76a1d67d09aed2fce709f1bc39c0cffb0
寇诗卉koush <koush@digiwin.com>
2019-09-12 15:35:01 +0800

    Revert "Revert "新增功能""
    This reverts commit d16b5a6aeab26f3641aeb6df454724d0f148067e.

---
+++ mp_kq/components/list/list_item.js
@@ -0,0 +1,58 @@
+// components/list/list_item.js
+Component({
+  relations: {
+    './list': {
+      type: 'parent', // 关联的目标节点应为父亲节点
+    }
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    icon:{
+      type: String,
+      value: ''
+    },
+    text:{
+      type: String,
+      value: ''
+    },
+    showExtr: {
+      type: Boolean,
+      value: false
+    },
+    value:{
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    index:0
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    /*
+    * 内部私有方法建议以下划线开头
+    * triggerEvent 用于触发事件
+    */
+    _tapItemEvent:function(){
+      const parent = this.getRelationNodes('./list')[0]
+      let current = {
+        data: {
+          text: this.data.text,
+          value: this.data.value,
+          showExtr: this.data.showExtr
+        },
+        index: this.data.index
+      }
+      parent ? parent.tapItemEvent(current) : this.triggerEvent('onTap', current)
+    }
+  }
+})
---
+++ mp_kq/pages/dialog/dialog.js
@@ -0,0 +1,98 @@
+// pages/dialog/dialog.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  showDialogFun: function () {
+    this.setData({
+      showDialog: true
+    })
+  },
+  showDialogFun2: function () {
+    this.setData({
+      showDialog2: true
+    })
+  },
+  cancelEvent: function (e) {
+    console.log(e)
+  },
+  confirmEvent: function (e) {
+    console.log(e)
+  },
+  openConfirm: function () {
+    wx.showModal({
+      title: '弹窗标题',
+      content: '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+      confirmText: "主操作",
+      cancelText: "辅助操作",
+      success: function (res) {
+        console.log(res);
+        if (res.confirm) {
+          console.log('用户点击主操作')
+        } else {
+          console.log('用户点击辅助操作')
+        }
+      }
+    });
+  },
+})
